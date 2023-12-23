08467a35d70fbaa10b7d67dcbb4f759ff68154f2
hideffrand <hideffrand@gmail.com>
2023-12-23 13:33:17 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import mobile from "./assets/deplix-mobile.png";
 import laptop from "./assets/deplix-laptop.png";
 import movies from "./assets/movies.png";
 import pc from "./assets/deplix-pc.png";
+import bottom from "./assets/bottom.png";
 
 function App() {
   useEffect(() => {
@@ -10,6 +11,11 @@ function App() {
     const mobileRef = document.querySelector(".mobile") as HTMLImageElement;
     const laptopRef = document.querySelector(".laptop") as HTMLImageElement;
     const pcRef = document.querySelector(".pc") as HTMLImageElement;
+
+    // Selector for mobile view
+    const mobile_mobileRef = document.querySelector(".mobile-mobile") as HTMLImageElement;
+    const mobile_laptopRef = document.querySelector(".mobile-laptop") as HTMLImageElement;
+    const mobile_pcRef = document.querySelector(".mobile-pc") as HTMLImageElement;
 
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
@@ -24,13 +30,14 @@ function App() {
         });
       }
 
-      laptopRef.style.transform = `translateY(-${
-        scrollPosition * 0.4
-      }px) translateX(-${scrollPosition * 0.04}px)`;
+      laptopRef.style.transform = `translateY(-${scrollPosition * 0.4}px) translateX(-${scrollPosition * 0.04}px)`;
       mobileRef.style.transform = `translateY(-${scrollPosition * 0.3}px)`;
-      pcRef.style.transform = `translateY(-${
-        scrollPosition * 0.4
-      }px) translateX(${scrollPosition * 0.04}px)`;
+      pcRef.style.transform = `translateY(-${scrollPosition * 0.4}px) translateX(${scrollPosition * 0.04}px)`;
+
+      // Animation for mobile view
+      mobile_laptopRef.style.transform = `translateY(-${scrollPosition * 0.04}px) scale(${1+ scrollPosition * 0.0001})`;
+      mobile_mobileRef.style.transform = `translateY(-${scrollPosition * 0.03}px) scale(${1+ scrollPosition * 0.0002})`;
+      mobile_pcRef.style.transform = `translateY(-${scrollPosition * 0.01}px) scale(${1+ scrollPosition * 0.0001})`;
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -69,8 +76,8 @@ function App() {
           <ion-icon name="chevron-down-outline"></ion-icon>
         </div>
       </div>
-      <main className="w-full h-screen bg-black flex items-center justify-around relative py-40">
-        <div className="text-white text-center absolute z-20 px-20">
+      <main className="w-full md:h-screen bg-black flex items-center justify-around relative py-40">
+        <div className="text-white text-center absolute px-10 md:px-20">
           <h1 className="font-bold text-2xl md:text-4xl">
             Watch it anywhere, on any device.
           </h1>
@@ -93,51 +100,41 @@ function App() {
         />
         <img src={pc} alt="Deplix Mock PC" className="pc w-1/2 md:w-1/4" />
       </section>
-      <section className="Deplix mock-section md:hidden w-full h-auto bg-black flex flex-col md:flex-row justify-center items-center gap-10">
+      <section className="mock-section md:hidden w-full h-auto bg-black flex flex-col md:flex-row justify-center items-center gap-10">
         <img
           src={laptop}
           alt="Deplix Mock Laptop"
-          className="laptop w-1/2 md:w-1/4"
+          className="mobile-laptop w-1/2 md:w-1/4"
         />
         <img
           src={mobile}
           alt="Deplix Mock Mobile"
-          className="mobile w-1/2 md:w-1/6"
+          className="mobile-mobile w-1/2 md:w-1/6"
         />
-        <img src={pc} alt="Deplix Mock PC" className="pc w-1/2 md:w-1/4" />
+        <img src={pc} alt="Deplix Mock PC" className="mobile-pc w-1/2 md:w-1/4" />
       </section>
-      <section className="w-full h-auto bg-black justify-center items-center flex flex-col md:flex-row py-40">
-        <div className="text-white md:w-1/2 ml-10 md:ml-0">
-          <h1 className="text-4xl">Ready to watch?</h1>
-          <p className="text-xl text-gray-300 md:w-1/2 my-10">
+      <section className="w-full h-screen bg-black justify-center items-center flex flex-col relative">
+        <div className="text-white text-center justify-center items-center flex flex-col translate-y-40">
+          <h1 className="text-2xl md:text-4xl font-bold">Ready to watch?</h1>
+          {/* <p className="text-xl md:text-2xl">
             Sign up now for free and enjoy our features to add your whatchlist,
             bookmarks, and favorite movies!
-          </p>
+          </p> */}
         </div>
-        <div className="flex gap-10">
-          <button className="bg-deplix-black text-white px-12 py-4 rounded-md uppercase font-bold">
-            Signup
-          </button>
-          <button className="bg-deplix-red text-white px-12 py-4 rounded-md uppercase font-bold">
-            Login
-          </button>
+        <div className="absolute flex gap-4">
+        <button className="bg-deplix-black md:px-10 px-4 py-2 text-sm rounded-md font-semibold text-white hover:bg-deplix-black hover:text-white hover:border hover:border-deplix-red">
+          LOGIN
+        </button>
+        <button className="bg-deplix-red md:px-10 px-4 py-2 text-sm rounded-md font-semibold text-white hover:bg-deplix-black hover:text-white hover:border hover:border-deplix-red">
+          SiGN UP
+        </button>
         </div>
+        <img src={bottom} alt="Deplix - Watch Movies Online" className="w-4/6"/>
       </section>
-      <footer className="text-gray-400 w-full h-auto bg-black flex justify-between px-4 md:px-20 py-10 text-xs md:text-md">
-        <p className="">Deplix @ 2023 | All Rights Reserved</p>
+      <footer className="text-gray-400 w-full h-auto bg-black flex justify-between px-4 md:px-20 py-4 text-xs md:text-md">
+        <p className="">Deplix | Inspired by <a className="underline" href="https://netflix.com">Netflix</a></p>
         <div className="flex flex-col items-end">
           by. hideffrand
-          <div className="flex gap-4">
-            <a className="hover:underline" href="https://github.com/hideffrand">
-              Github
-            </a>
-            <a
-              className="hover:underline"
-              href="https://www.linkedin.com/in/deffrand-farera/"
-            >
-              LinkedIn
-            </a>
-          </div>
         </div>
       </footer>
     </>
